a660b6760003f9a54faa169ea96491d4bdaea112
Bruno Germano <bruno.germano@iceland2.com>
2012-08-27 16:06:33 -0300

    grow a fix release version

--- bin/stylus-spriting.js
+++ bin/stylus-spriting.js
@@ -1,35 +1,35 @@
 #!/usr/bin/env node
 
 var program = require('commander'),
-	args = process.argv.slice(2),
-	sprite = require('../lib/sprite').sprite;
+    args = process.argv.slice(2),
+    sprite = require('../lib/sprite').sprite;
 
 program
-	.version('0.0.2')
-	.usage('path/to/images/*.png [options]')
-	.option('--image <file>', 'Name for output image.')
-	.option('--stylus <file>', 'Name for output stylus.')
-	.option('--class-prefix <string>', 'Class prefix for stylus.')
-	.option('--debug <boolean>', 'Debugger mode.');
+    .version('0.0.3')
+    .usage('path/to/images/*.png [options]')
+    .option('--image <file>', 'Name for output image.')
+    .option('--stylus <file>', 'Name for output stylus.')
+    .option('--class-prefix <string>', 'Class prefix for stylus.')
+    .option('--debug <boolean>', 'Debugger mode.');
 
 program
-  	.command('*')
-	.action(function(){
-		var images = Array.prototype.slice.call(arguments, 0);
-		images.pop();
-		
-		var imgOutput = program.image?program.image:"sprite.png",
-			stylusOutput = program.stylus?program.stylus:"sprite.styl",
-			debug = program.debug?program.debug==='true'?true:false:false,
-			classPrefix = program.classPrefix?program.classPrefix:'sprite-';
+    .command('*')
+    .action(function(){
+        var images = Array.prototype.slice.call(arguments, 0);
+        images.pop();
+        
+        var imgOutput = program.image?program.image:"sprite.png",
+            stylusOutput = program.stylus?program.stylus:"sprite.styl",
+            debug = program.debug?program.debug==='true'?true:false:false,
+            classPrefix = program.classPrefix?program.classPrefix:'sprite-';
 
-		if(debug)
-			console.log('[stylus-spriting] - Start process in Debugger mode.');
-		else
-			console.log('[stylus-spriting] - Start process.');
+        if(debug)
+            console.log('[stylus-spriting] - Start process in Debugger mode.');
+        else
+            console.log('[stylus-spriting] - Start process.');
 
-		new sprite(images, imgOutput, stylusOutput, classPrefix, debug).process();
-	});
+        new sprite(images, imgOutput, stylusOutput, classPrefix, debug).process();
+    });
 
 
 program.parse(process.argv);
